3207f1be209b55ab2bff7b84ab920d97170ba4c7
Sivavenugopal <137591736+lightspeed123@users.noreply.github.com>
2026-05-28 11:33:31 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const workoutRoutes = require('./routes/workouts.js')
+
+const app = express()
+
+//middleware - code that executes between req and res
+/* next() - Middleware can call the next function to pass control to the next middleware in the chain or to 
+the next route handler. This allows multiple middleware functions to sequentially process the request and response */
+app.use((req,res,next)=>{   //global middleware
+    console.log(req.path,req.method)
+    next()
+})
+
+app.use(cors())
+
+//routes
+app.use(express.json()) // allows us to access the info in the body of the req
+
+app.use('/api/workouts' ,workoutRoutes)
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    //listen for requests
+    if(process.env.PORT)
+        app.listen(process.env.PORT, ()=>{
+            console.log("Connected to DB & Server's Working on port "+process.env.PORT)
+        })
+}).catch((err)=>{
+    console.log(err)
+})
+
+module.exports = app
